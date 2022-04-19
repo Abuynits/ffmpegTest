21788ac2e36f43bb26b755d1b466f653c2b84b47
Alexiy Buynitsky <abuynits@gmail.com>
2022-04-18 23:09:15 -0700

    initial commot

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include <libavutil/mem.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavutil/frame.h>
+using namespace std;
+int main() {
+    const char *outfilename, *filename;
+    const AVCodec *codec;
+    AVCodecContext *c= NULL;
+    AVCodecParserContext *parser = NULL;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
