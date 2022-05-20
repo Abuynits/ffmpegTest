4e2327ab29ec695c2231c78b55159135363ea90e
Alexiy Buynitsky <abuynits@gmail.com>
2022-05-20 12:53:10 -0700

    BROKEN: working on an audio resampler, added another file

---
+++ Resampler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexiy Buynitsky on 5/20/22.
+//
+
+#include "Resampler.h"
---
+++ Resampler.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexiy Buynitsky on 5/20/22.
+//
+
+#ifndef FFMPEGTEST5_RESAMPLER_H
+#define FFMPEGTEST5_RESAMPLER_H
+
+
+
+class Resampler {
+
+};
+
+
+
+#endif //FFMPEGTEST5_RESAMPLER_H
